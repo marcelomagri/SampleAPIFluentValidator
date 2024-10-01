5c7b36f9a0515976c8bc545f5629cd32703523c1
Marcelo Nogueira Magri <marcelo_magri@hotmail.com>
2024-10-01 14:57:51 -0300

    Inclusão do swagger no endereço <url do IIS ou IIS Express>/swagger

--- SampleAPI/Program.cs
+++ SampleAPI/Program.cs
@@ -2,12 +2,28 @@ using FluentValidation;
 using FluentValidation.AspNetCore;
 using SampleAPI.Models;
 using System.Globalization;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddValidatorsFromAssemblyContaining<Program>();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Minimal API c/ Fluent Validation", Version = "v1" });
+});
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Minimal API c/ Fluent Validation v1");
+        c.RoutePrefix = "swagger";
+    });
+}
 
 app.MapPost("/pedido/incluir", (Pedido pedido, IValidator<Pedido> validator) => {
 
